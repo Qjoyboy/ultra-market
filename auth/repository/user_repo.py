0bac7e22e30eec08270a070979090d55063e7c07
Qjoyboy <khanmagomedovruslan77@gmail.com>
2026-03-24 00:29:15 +0300

    feat: created auth-user repo

---
+++ auth/repository/user_repo.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from db.models import User
+
+async def create_user(session: AsyncSession, email: str, password_hash: str) -> User:
+    user = User(email=email, password_hash=password_hash)
+    session.add(user)
+
+    await session.commit()
+    await session.refresh()
+
+    return user
+
+async def get_by_email(session: AsyncSession, email:str) -> User | None:
+    result = await session.execute(
+        select(User).where(User.email == email)
+    )
+
+    return result.scalar_one_or_none()
+
+
+async def get_by_id(session: AsyncSession, user_id: int) -> User | None:
+    result = await session.execute(
+        select(User).where(User.id == user_id)
+    )
+
+    return result.scalar_one_or_none()
